84204ed43193500308903db6cf808cb076eebf8b
UniverseBC <44294510+UniverseBC@users.noreply.github.com>
2018-10-22 16:47:52 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,6 +2,8 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const prefix = '$'
 
+
+
 client.on('ready',  () => {
   console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'); 
   console.log('by Universe');
